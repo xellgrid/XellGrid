eb034d37f83a2b7156404e37f3ec1510838d96cc
xellgrid <105471957+xellgrid@users.noreply.github.com>
2022-05-15 23:07:24 -0400

    Update gitignore refactor gridpy (#4)
    * minor changes to grid.py
    
    * modify_gitignore_and_refactor_grid_py_XellGrid

--- qgrid/grid.py
+++ qgrid/grid.py
@@ -24,6 +24,8 @@ from itertools import chain
 from uuid import uuid4
 from six import string_types
 
+from .grid_default_settings import _DefaultSettings
+
 # versions of pandas prior to version 0.20.0 don't support the orient='table'
 # when calling the 'to_json' function on DataFrames.  to get around this we
 # have our own copy of the panda's 0.20.0 implementation that we use for old
@@ -33,73 +35,6 @@ from six import string_types
 #    import pandas.io.json as pd_json
 # else:
 #    from . import pd_json
-
-
-class _DefaultSettings(object):
-
-    def __init__(self):
-        self._grid_options = {
-            'fullWidthRows': True,
-            'syncColumnCellResize': True,
-            'forceFitColumns': True,
-            'defaultColumnWidth': 150,
-            'rowHeight': 28,
-            'enableColumnReorder': False,
-            'enableTextSelectionOnCells': True,
-            'editable': True,
-            'autoEdit': False,
-            'explicitInitialization': True,
-            'maxVisibleRows': 15,
-            'minVisibleRows': 8,
-            'sortable': True,
-            'filterable': True,
-            'highlightSelectedCell': False,
-            'highlightSelectedRow': True,
-            'boldIndex': True
-        }
-        self._column_options = {
-            'editable': True,
-            # the following options are supported by SlickGrid
-            'defaultSortAsc': True,
-            'maxWidth': None,
-            'minWidth': 30,
-            'resizable': True,
-            'sortable': True,
-            'toolTip': "",
-            'width': None
-        }
-        self._show_toolbar = True
-        self._precision = None  # Defer to pandas.get_option
-
-    def set_grid_option(self, optname, optvalue):
-        self._grid_options[optname] = optvalue
-
-    def set_defaults(self, show_toolbar=None, precision=None,
-                     grid_options=None, column_options=None):
-        if show_toolbar is not None:
-            self._show_toolbar = show_toolbar
-        if precision is not None:
-            self._precision = precision
-        if grid_options is not None:
-            self._grid_options = grid_options
-        if column_options is not None:
-            self._column_options = column_options
-
-    @property
-    def show_toolbar(self):
-        return self._show_toolbar
-
-    @property
-    def grid_options(self):
-        return self._grid_options
-
-    @property
-    def precision(self):
-        return 6
-
-    @property
-    def column_options(self):
-        return self._column_options
 
 
 class _EventHandlers(object):
@@ -139,7 +74,7 @@ def set_defaults(show_toolbar=None,
                  grid_options=None,
                  column_options=None):
     """
-    Set the default qgrid options.  The options that you can set here are the
+    Set the default XellGrid options.  The options that you can set here are the
     same ones that you can pass into ``QgridWidget`` constructor, with the
     exception of the ``df`` option, for which a default value wouldn't be
     particularly useful (since the purpose of qgrid is to display a DataFrame).
