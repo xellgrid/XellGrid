608624847141f8dbf7c26d97e5dbd1b8eefa10f9
xellgrid <105471957+xellgrid@users.noreply.github.com>
2022-05-22 15:49:34 -0400

    addressing comments, reorganization and clean up

--- qgrid/__init__.py
+++ qgrid/__init__.py
@@ -1,15 +1,24 @@
 from ._version import version_info, __version__  # noqa F401
 
 from .grid import (
-    enable,
-    disable,
-    set_defaults,
-    on,
-    off,
-    set_grid_option,
-    show_grid,
     QgridWidget,
     QGridWidget,
+)
+
+from .grid_default_settings import (
+    set_defaults,
+    set_grid_option,
+)
+
+from .grid_event_handlers import (
+    on,
+    off
+)
+
+from .grid_display_options import (
+    enable,
+    disable,
+    show_grid,
 )
 
 
