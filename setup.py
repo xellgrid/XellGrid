d6cdb987cb2eedfe971058c2240ac8ccfdc17beb
warm200 <wm140906@gmail.com>
2023-03-05 21:46:56 -0500

    test lib change

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup_args = dict(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=read_requirements('requirements.txt'),
+    install_requires=read_requirements('requirements-dev.txt'),
     zip_safe=False,
     include_package_data=True,
     python_requires=">=3.6",
